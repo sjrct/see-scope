006833f3afb1e2ff44c0dcc2e120813f61bacdc3
Chris Harding <sjrct@users.noreply.github.com>
2014-03-24 13:19:58 -0400

    Removed unnecessary console.log

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log('clicked');
 	// FIXME injecting js/css twice
 	chrome.tabs.executeScript({ file: "seescope.js" });
 	chrome.tabs.insertCSS({ file: "seescope.css" });
